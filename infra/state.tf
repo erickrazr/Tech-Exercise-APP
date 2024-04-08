705ebbbff1dfeacfcac5310079eb0b2113041950
Erick Ferreira <erickrazr@gmail.com>
2024-04-07 22:07:10 -0300

    Update state.tf

--- infra/state.tf
+++ infra/state.tf
@@ -1,6 +1,6 @@
 terraform {
  backend "s3" {
-   bucket         = ""te-env-lab
+   bucket         = "te-env-lab"
    key            = "tfplan"
    region         = "us-east-1"
    encrypt        = false
